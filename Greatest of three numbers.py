d2f2a5968490a0f9285d759e8737aca952d72e41
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-08-23 22:13:57 +0530

    Add files via upload
    Greatest of three numbers in python

---
+++ Greatest of three numbers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#Greatest of three numbers
+
+a=int(input("Enter the value:"))
+b=int(input("Enter the value:"))
+c=int(input("Enter the value:"))
+if (a>b) and (a>c):
+    print("a is greater")
+elif (b>a) and (b>c):
+    print("b is greater")
+else:
+    print("c is greater")
+
+
+# In[ ]:
+
+
+
+
