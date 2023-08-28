d2ff3a6fbda738be7edc19038d4eeacbed8cb705
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-08-28 22:17:02 +0530

    Add files via upload
    To find the factorial of a given number

---
+++ Factorial of a number.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Factorial of a number
+
+n=int(input("Enter the number:"))
+factorial=1
+while (n>0):
+    factorial=factorial*n
+    n-=1
+print("Factorial of the number is",factorial)
+    
+
+
+# In[ ]:
+
+
+
+
