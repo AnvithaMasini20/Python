e9a14d5b6adcdd1f55d6f7f3d14a45b30e8e7d3f
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-03 14:25:47 +0530

    Add files via upload

---
+++ Basic programs.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("anvitha")
+
+
+# In[2]:
+
+
+a="anvitha"
+print(a)
+
+
+# In[3]:
+
+
+a=2
+b=3
+print(a+b)
+
+
+# In[4]:
+
+
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+print("Addition of two numbers is",a+b)
+
+
+# In[5]:
+
+
+a=6
+b=8
+print(a-b)
+
+
+# In[6]:
+
+
+a=4
+b=6
+print(a*b)
+
+
+# In[7]:
+
+
+a=20
+b=4
+print(a/b)
+
+
+# In[8]:
+
+
+a=5
+b=9
+print("The sum of a and b",a+b)
+
+
+# In[9]:
+
+
+name="Hello,Anvitha!"
+print(name)
+
+
+# In[10]:
+
+
+pi=3.14159
+print(pi)
+
+
+# In[11]:
+
+
+is_raining="True"
+print(is_raining)
+is_raining=True
+print(type(is_raining))
+
+
+# In[14]:
+
+
+sentence="I'm Anvitha Masini studying Btech third year"
+a=int(input("Enter a number:"))
+print(a*sentence)
+
+
+# In[20]:
+
+
+x=4
+y=2
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x^y)
+print(x%y)
+print(x//y)
+value=42
+print(value)
+a=int(input("Enter new variable:"))
+print("The updated value of the variable",a)
+
+
+# In[ ]:
+
+
+
+
