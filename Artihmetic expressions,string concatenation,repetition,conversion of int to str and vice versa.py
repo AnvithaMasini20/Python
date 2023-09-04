d6cba02274f3e6c8a30f02d877c2585c7c35cc53
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-04 10:39:25 +0530

    Add files via upload

---
+++ Artihmetic expressions,string concatenation,repetition,conversion of int to str and vice versa.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#1
+result=10+3*2-8/4
+print(result)
+
+#2
+result=4**2+5/2*3
+print(result)
+
+#3
+result=(8+4)*3/2
+print(result)
+
+#4
+result=16/4+2**3-6
+print(result)
+
+#5
+result=10-3*(4+2)/5
+print(result)
+
+
+# In[7]:
+
+
+#String concatenation
+str1="Hello"
+str2="World"
+print(str1+str2)
+
+
+# In[9]:
+
+
+a="Anvitha"
+b="Hello"
+print(b+a)
+
+
+# In[10]:
+
+
+#Repetition
+word="Python"
+print(word*5)
+
+
+# In[12]:
+
+
+word="Python Programming"
+number=5
+print(word*number)
+
+
+# In[20]:
+
+
+sentence="This is a demo class ."
+print(len(sentence))
+
+
+# In[35]:
+
+
+value=42
+result=str(value)
+print(result)
+print(type(result))
+
+
+# In[36]:
+
+
+value="123"
+result=int(value)
+print(result)
+
+
+# In[ ]:
+
+
+
+
