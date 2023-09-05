6b554073190c24cc17e46803310eb1eb2b9cb7c9
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-05 19:28:24 +0530

    Add files via upload

---
+++ Indexing the strings.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#To get the first character 
+a="The sun is shining."
+print(a[0])
+
+
+# In[5]:
+
+
+#To get the last character
+b="She sells seashells by the seashore."
+print(b[-1])
+
+
+# In[6]:
+
+
+#To get the character at index 3
+c="I love python"
+print(c[3])
+
+
+# In[8]:
+
+
+#To get the second last character
+d="Life is beautiful."
+print(d[-2])
+
+
+# In[9]:
+
+
+#To get index from 7 to 14(exclusive)
+d="Welcome to Python programming."
+print(d[7:14])
+
+
+# In[14]:
+
+
+#To get the index from -9 to -3
+x="The future is bright."
+print(x[-9:-3])
+
+
+# In[16]:
+
+
+#To get the first six characters
+y="Good things take time."
+print(y[0:6])
+
+
+# In[20]:
+
+
+a="Python is awesome!"
+print(a[::-1])
+
+
+# In[ ]:
+
+
+
+
