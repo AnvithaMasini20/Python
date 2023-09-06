7eb0b106147e2b3f55e67a6a1e33d2b018622c71
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-06 18:06:40 +0530

    Add files via upload

---
+++ Comparison operators.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Greater than operator(>)
+
+a=5
+b=3
+print(a>b)
+
+
+# In[2]:
+
+
+#Less than operator(<)
+
+x=10
+y=20
+print(x<y)
+
+
+# In[4]:
+
+
+#Greater than or equal to operator(>=)
+
+m=7
+a=7
+print(m>=a)
+
+
+# In[5]:
+
+
+#Less than or equal to operator(<=)
+
+a=15
+m=12
+print(a<=m)
+
+
+# In[6]:
+
+
+#Equal to operator(==)
+
+x=10
+y=10
+print(x==y)
+
+
+# In[7]:
+
+
+#Equal to operator(==)
+
+x="hello world"
+y="hello world"
+print(x==y)
+
+
+# In[8]:
+
+
+#Not equal to operator(!=)
+
+a=10
+b=20
+print(a!=b)
+
+
+# In[13]:
+
+
+#Comparing integers and floats
+
+integers=4
+floats=5.0
+print(integers>floats)
+
+
+# In[ ]:
+
+
+
+
