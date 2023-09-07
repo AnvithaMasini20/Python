9c1292ed9ff7cfd8b620b1d1315369e577683f21
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-07 20:46:01 +0530

    Add files via upload

---
+++ Logical operators (and,or,not).py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#'and' operator with two True conditions
+
+a=(10>5)
+b=("apple"=="apple")
+print(a and b)
+
+
+# In[6]:
+
+
+#'and' operator with one False condition
+
+a=(3<2)
+b=("banana"=="orange")
+print(a and b)
+
+
+# In[8]:
+
+
+#'and' operator with one True and one False condition
+
+a=(5>=3)
+b=(10!=10)
+print(a and b)
+
+
+# In[10]:
+
+
+#'or' operator with two True conditions
+
+a=("car"=="car")
+b=(6<9)
+print(a or b)
+
+
+# In[11]:
+
+
+#'or' operator with two False conditions
+
+a=(2==3)
+b=(8<3)
+print(a or b)
+
+
+# In[13]:
+
+
+#'not' operator with True condition
+
+a=not(4<=3)
+print(a)
+
+
+# In[ ]:
+
+
+
+
