917de2363a09acbb3d4fcfa0267d270f69acdc27
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-09 21:05:57 +0530

    Add files via upload

---
+++ while and for loop.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a while loop that prints numbers from 1 to 10 
+
+i=1
+n=10
+while i<=10:
+    print(i)
+    i=i+1
+
+
+# In[5]:
+
+
+#Create a while loop that calculates the sum of numbers from 1 to n,where n is the input.
+
+n=int(input("Enter a number:"))
+i=1
+sum=0
+while i<=n:
+    sum=sum+i
+    i=i+1
+print(sum)
+
+
+# In[14]:
+
+
+#Write a for loop that prints all numbers from 1 t0 5
+
+for i in range(1,6):
+    print(i)
+
+
+# In[15]:
+
+
+#Use a for loop to calculate the sum of numbers from 1 to 10
+
+sum=0
+for i in range(1,11):
+    sum=sum+i
+    print(sum)
+
+
+# In[ ]:
+
+
+
+
