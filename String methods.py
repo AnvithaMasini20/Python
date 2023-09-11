3f8dc9396ce51558c49edb3d0af23c2822a03ea6
AnvithaMasini20 <116957112+AnvithaMasini20@users.noreply.github.com>
2023-09-11 20:02:45 +0530

    Add files via upload

---
+++ String methods.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#Write a python program that takes a user input string and converts it into lowercase using lower() method
+
+a="HELLO WORLD!"
+b=a.lower()
+print(b)
+
+
+# In[9]:
+
+
+#Given a string "Hello, World!",find it's length using len() method
+
+a="Hello, World!"
+b=len(a)
+print(b)
+
+
+# In[11]:
+
+
+#Write a python program that takes a user input string and converts it into uppercase using upper() method
+
+a="hello world!"
+b=a.upper()
+print(b)
+
+
+# In[12]:
+
+
+#Given a string "Hello123", check if it contains only alphabetic characters using the isalpha() method
+
+a="Hello123"
+b=a.isalpha()
+print(b)
+
+
+# In[17]:
+
+
+#Write a python program that takes user input string with leading& trailing whitespaces& removes them using the strip() method 
+
+a="    Python Programming    "
+print(a.strip())
+print(a.strip("Python"))
+
+
+
+# In[ ]:
+
+
+
+
